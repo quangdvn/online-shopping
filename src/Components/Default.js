34c74d494e497db71337f773b1a30e569597511e
quangdvn <ghitacluber@gmail.com>
2019-03-31 23:55:52 +0700

    Build default page + Test Netlify deployment

--- src/Components/Default.js
+++ src/Components/Default.js
@@ -3,8 +3,21 @@ import React, { Component } from "react";
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>Page not found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-uppercase pt-5">
+            <h1 className="text-title display-3 text-danger">404 error</h1>
+            <h2>Page Not Found</h2>
+            <h4>
+              the requested URL
+              <span className="text-danger">
+                {" "}
+                {this.props.location.pathname}
+              </span>{" "}
+              was not found
+            </h4>
+          </div>
+        </div>
       </div>
     );
   }
