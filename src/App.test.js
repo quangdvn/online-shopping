8a86f4f7e9396f969699b2abbbc0b00e499d4429
Manh Hung <hungdm1211@gmail.com>
2019-05-04 12:09:15 +0700

    fix test failed

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {Provider} from 'react-redux'
+import store from './redux/store'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+    const div = document.createElement('div')
+    ReactDOM.render(
+        <Router history={history}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Router>,
+        div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+})
