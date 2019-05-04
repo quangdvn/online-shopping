b10fe637f0050443a34cb3d02fcfc56b7218acec
Manh Hung <hungdm1211@gmail.com>
2019-05-04 12:16:23 +0700

    cmon

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import {Provider} from 'react-redux'
 import store from './redux/store'
+import {Router} from 'react-router-dom'
+import history from './utils/history'
+import {ProductProvider} from './ProductContext'
 
 it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(
+        <Router history={history}>
             <Provider store={store}>
-                <App />
-            </Provider>,
+                <ProductProvider>
+                    <App />
+                </ProductProvider>
+            </Provider>
+        </Router>,
         div
     )
     ReactDOM.unmountComponentAtNode(div)
