dc51d3bb53c5b9382acfd054194717255b1c6c9d
Manh Hung <hungdm1211@gmail.com>
2019-05-04 12:11:17 +0700

    fix test failed

--- src/App.test.js
+++ src/App.test.js
@@ -7,11 +7,9 @@ import store from './redux/store'
 it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(
-        <Router history={history}>
             <Provider store={store}>
                 <App />
-            </Provider>
-        </Router>,
+            </Provider>,
         div
     )
     ReactDOM.unmountComponentAtNode(div)
