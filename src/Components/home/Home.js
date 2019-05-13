45ab43f8e3306ec3fe0d8296f50397f2194d4ef5
Manh Hung <hungdm1211@gmail.com>
2019-05-13 07:37:14 +0700

    remove our team

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
             <div>
                 <Banner/>
                 <ProductList />
-                <OurTeam/>
+                {/* <OurTeam/> */}
             </div>
         )
     }
